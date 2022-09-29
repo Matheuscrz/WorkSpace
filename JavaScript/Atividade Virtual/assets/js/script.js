1e5daa699434e944d62e06497d57df642a90654f
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-09-28 22:44:58 -0300

    Atividade

---
+++ JavaScript/Atividade Virtual/assets/js/script.js
@@ -0,0 +1,9 @@
+/* Matheus Lima da Cruz*/
+
+//Array que armazena Carga Horaria
+const CH = []
+
+//Função que calcula a carga horaria total
+const chTotal = (x) => {
+    return x.reduce((x,acc ) => x + acc ,0)
+}
