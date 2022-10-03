b71907a8addf8bc01d4d3c08b5c3b61a0254a95c
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-10-02 21:49:13 -0300

    Atividade

--- JavaScript/Atividade Virtual/assets/js/script.js
+++ JavaScript/Atividade Virtual/assets/js/script.js
@@ -1,24 +1,51 @@
 /* Matheus Lima da Cruz*/
+var Ano = []
+var Codigo = []
+var CH = []
+var Freq = []
+var Nota = []
 
-//Array que armazena Carga Horaria
-const CH = []
+const dados = () => {
+    Ano.push(document.getElementById("ano").value)
+    Codigo.push(document.getElementById("codigo").value)
+    CH.push(document.getElementById("ch").value)
+    Freq.push(document.getElementById("freq").value)
+    Nota.push(document.getElementById("nota").value)
+}
+
+//Função que calcula o tempo de curso em períodos
+const tempoCurso = (x) => {
+    const array = [...new Set(x)]
+    const aux = array.map(p => p**0).reduce((p,acc)=> p + acc, 0)
+    return aux
+}
 
 //Função que calcula a carga horaria total
 const chTotal = (x) => {
     return x.reduce((x,acc ) => x + acc ,0)
 }
 
-const ano_periodo = []
-
-const tempoCurso = (x) => {
-
+//Função que calcula a média geral ponderada
+//OBS: Não comsegui fazer a função de media ponderada sem utilizar laços for, while
+const mediaPonterada = (x) => {
+    const total = x.reduce((p,acc)=> p + acc,0)
+    const aux = x.map(p => p**0).reduce((p,acc) => p + acc, 0)
+    return total/aux
 }
 
-const mediaPonterada = (x,y) => {
-    const mult = (x*y)
+const DesvioPad = (x,y) => {
+    const aux = x.map(x => (x-y)**2).reduce((p,acc) => p + acc, 0)
+    const n = x.map(p => p**0).reduce((p,acc)=> p + acc, 0) - 1
+    return Math.sqrt(aux/n)
 }
-mediaPonterada([1,2,3,4,5],[2,2,2,2,2])
+console.log(DesvioPad([7.2,8,4.5,6.7,5.5],6.38))
 
-const listaDiciplinas = (x) => {
-    
+//Função que lista as disciplinas aprovadas
+const listaDiciplinasAprov = (x,y) => {
+    const aux = x.filter(x => x >= 5)
+}
+
+//Função que lista as disciplinas reprovadas
+const listaDiciplinasReprov = (x,y) => {
+    const aux = x.filter(x => x <5)
 }
