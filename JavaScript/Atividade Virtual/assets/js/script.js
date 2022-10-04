bf2e0e48d0c0fcf79a715d4cca5ca8d57dc3d0cc
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-10-03 22:31:55 -0300

    Atividade

--- JavaScript/Atividade Virtual/assets/js/script.js
+++ JavaScript/Atividade Virtual/assets/js/script.js
@@ -1,18 +1,28 @@
 /* Matheus Lima da Cruz*/
 var Ano = []
 var Codigo = []
-var CH = []
+var CargaH = []
 var Freq = []
 var Nota = []
 
 const dados = () => {
-    Ano.push(document.getElementById("ano").value)
-    Codigo.push(document.getElementById("codigo").value)
-    CH.push(document.getElementById("ch").value)
-    Freq.push(document.getElementById("freq").value)
-    Nota.push(document.getElementById("nota").value)
+    var a = document.getElementById("ano").value
+    Ano.push(a)
+    var b = document.getElementById("codigo").value
+    Codigo.push(b)
+    var c = document.getElementById("ch").value
+    CargaH.push(c)
+    var d = document.getElementById("freq").value
+    Freq.push(d)
+    var e = document.getElementById("nota").value
+    Nota.push(e)
 }
 
+//Função que converte os valores de String para Double
+const converte = (x) => {
+    const array =  x.map(Number)
+    return array
+}
 //Função que calcula o tempo de curso em períodos
 const tempoCurso = (x) => {
     const array = [...new Set(x)]
@@ -38,7 +48,6 @@ const DesvioPad = (x,y) => {
     const n = x.map(p => p**0).reduce((p,acc)=> p + acc, 0) - 1
     return Math.sqrt(aux/n)
 }
-console.log(DesvioPad([7.2,8,4.5,6.7,5.5],6.38))
 
 //Função que lista as disciplinas aprovadas
 const listaDiciplinasAprov = (x,y) => {
@@ -48,4 +57,13 @@ const listaDiciplinasAprov = (x,y) => {
 //Função que lista as disciplinas reprovadas
 const listaDiciplinasReprov = (x,y) => {
     const aux = x.filter(x => x <5)
+}  
+
+const Processar = () => {
+    const p1 = tempoCurso(converte(Ano))
+    const p2 = mediaPonterada(converte(Nota)).toFixed(2)
+    const p3 = DesvioPad(converte(Nota),p2).toFixed(2)
+    const p4 = chTotal(converte(CargaH))
+    const resultado = `Tempo de Curso em Períodos: ${p1} \n Media: ${p2} \n Desvio Padrão: ${p3} \n Carga Horaria Total: ${p4}`
+    document.getElementById("output").innerHTML = resultado
 }
